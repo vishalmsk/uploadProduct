750c7ae8067853d72e48972f0d6fdf7f75cf89b4
vishalmsk <97456349+vishalmsk@users.noreply.github.com>
2023-07-13 17:14:03 +0530

    Sifter 16913 and 16534
    resource code upated in TOC and pacing information code added by Mitesh
    Also code files cleaned and unwanted code removed

--- templates/excelResource/httpAxioRequest.js
+++ templates/excelResource/httpAxioRequest.js
@@ -1,48 +1,38 @@
 import axios from 'axios';
+/*
+##########################################################################
+Important Points while updating this file: 
+** Please do not add any external module in this File. 
+** This file is used in external Node application as mentioend in the tickete Sifter 16450
+** Addition of external modules will create issues in node application.
+** If you want to write any function, please add function in the same file.
+** Please do not use console.log, use function _console and make enableConsole variable false after work is complete., Because console data output by this function is read and processed in node appliction. Unwanted console output will create problem in node processing.
+##########################################################################
+*/
 export function getHttpRequest(settings, _callBackFn, _errorCallBackFn) {
-    //console.log("getHttpRequest from httpAxio");
-    //console.log("settings = ", settings);
-    //console.log("applicationType = ",applicationType);
-    if(settings.applicationType == "react")
-    {
-      let request = $.ajax(settings);
-      request.done(function(data) {
-          if (typeof _callBackFn !== "undefined") {
-              _callBackFn(data);
-              request = null;
-          }
+  if (settings.applicationType == "react") {
+    let request = $.ajax(settings);
+    request.done(function (data) {
+      if (typeof _callBackFn !== "undefined") {
+        _callBackFn(data);
+        request = null;
+      }
+    });
+    request.fail(function () {
+      if (typeof _errorCallBackFn !== "undefined") {
+        _errorCallBackFn(request.status);
+      }
+      request = null;
+    });
+  }
+  else {
+    axios(settings).then(function (response) {
+      _callBackFn(response.data);
+    })
+      .catch(function (error) {
+        _errorCallBackFn(error);
       });
-      request.fail(function() {
-          if (typeof _errorCallBackFn !== "undefined") {
-              _errorCallBackFn(request.status);
-          }
-          request = null;
-      });
-    }
-    else
-    {
-      axios(settings).then(function (response) {
-         // console.log(response);
-          _callBackFn(response.data);
-         })
-         .catch(function (error) {
-          //console.log(error);
-          _errorCallBackFn(error);
-         });
-    }
-   
-    //=====================================
-    /*
-  axios({
-   method: 'PUT',
-   url: "https://node.perfectionnext.com:8083/usercontent/query",
-   data:data
-  }).then(function (response) {
-   console.log(response);
-  })
-  .catch(function (error) {
-   console.log(error);
-  });
-  */
-    //======================================
-  }; 
+  }
+}; 
+
+//===========================================================
